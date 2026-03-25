38a0341ba9c5a83fce047fc832f8ab1015cadcd2
lam3487 <lam3487@kent.ac.uk>
2026-03-25 20:40:14 +0000

    Card class

--- BlackJack.py
+++ BlackJack.py
@@ -1,12 +1,26 @@
 import random
 
+class Main:
+    def __init__(self):
+        self.Deck = []
+
+    def Deal(self):
+        print(cards)
+
 class Deck:
     Suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
     Values = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
 
     def __init__(self):
-        self.cards = []
-    
+        self.Cards = []
+
     def __len__(self):
         return len(self.cards)
     
+
+class Card:
+    def __init__(self, Suit, Value):
+        self.Suit = Suit
+        self.Value = Value
+    
+        
