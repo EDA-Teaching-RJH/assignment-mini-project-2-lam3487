57e946092b3aeb92065c4c6ddc7934e672a23159
lam3487 <lam3487@kent.ac.uk>
2026-03-26 14:45:10 +0000

    start of PlayGame, Player and added a shuffling to the deck

--- BlackJack.py
+++ BlackJack.py
@@ -3,10 +3,17 @@ import random
 class Game:
     def __init__(self):
         self.Deck = []
+
+    def PlayGame(self):
+        self.Deck = Deck()
+        self.Deck.CreateDeck()
+        self.Deck.ShuffleDeck()
+
     def Test(self):
         self.Deck = []
         self.Deck = Deck()
         deck = self.Deck.CreateDeck()
+        deck = self.Deck.ShuffleDeck()
         for n, card in enumerate(deck):
             print(deck[n])
         
@@ -25,7 +32,11 @@ class Deck:
             for Value in self.Values:
                 self.Cards.append(Card(Suit, Value))
         return(self.Cards)
-
+     
+    def ShuffleDeck(self):
+        random.shuffle(self.Cards)
+        return(self.Cards)
+    
 class Card:
     def __init__(self, Suit, Value):
         self.Suit = Suit
@@ -33,7 +44,20 @@ class Card:
     
     def __str__(self):
         return str(f"{self.Value} of {self.Suit}")
-    
+
+    def GetCardScore(self):
+        if self.Value == "jack" or self.Value == "queen" or self.Value == "king":
+            return 10
+        if self.Value in ["2", "3", "4", "5", "6", "7", "8", "9", "10"]:
+            return(self.Value)
+        if self.Value == "ace":
+            return 1
+        
+class Player:
+    def __init__(self):
+        self.Hand = []
+
+
 def main():
     game = Game()
     game.Test()
