7b07ceb4217f900f29d90682f6340fb3e30fe85e
lam3487 <lam3487@kent.ac.uk>
2026-03-26 14:08:15 +0000

    test the deck creation however each card is output as <__main__.Card object at 0x7c4ffec6f860>

--- BlackJack.py
+++ BlackJack.py
@@ -1,12 +1,14 @@
 import random
 
-class Main:
+class Game:
     def __init__(self):
         self.Deck = []
-
-    def Deal(self):
-        print(cards)
-
+    def Test(self):
+        self.Deck = []
+        self.Deck = Deck()
+        deck = self.Deck.CreateDeck()
+        print(deck)
+        
 class Deck:
     Suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
     Values = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
@@ -17,10 +19,19 @@ class Deck:
     def __len__(self):
         return len(self.cards)
     
+    def CreateDeck(self):
+        for Suit in self.Suits:
+            for Value in self.Values:
+                self.Cards.append(Card(Suit, Value))
+        return(self.Cards)
 
 class Card:
     def __init__(self, Suit, Value):
         self.Suit = Suit
         self.Value = Value
     
-        
+def main():
+    game = Game()
+    game.Test()
+
+main()
